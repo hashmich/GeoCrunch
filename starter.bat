59cd9df2b65e1f5c0787f04a80e58187d1c451a9
hashmich <mail@hendrikschmeer.de>
2017-03-15 14:21:33 +0100

    updated starter.bat

--- starter.bat
+++ starter.bat
@@ -2,7 +2,7 @@
 
 
 
-:: java -Dhttp.proxyHost=proxy -Dhttp.proxyPort=8080 -jar geoCrunch.jar
+:: java -Xmx1g -Dhttp.proxyHost=proxy -Dhttp.proxyPort=8080 -jar geoCrunch.jar
 java -Xmx1g -jar geoCrunch.jar
 
 @echo press any key to exit
